bbf74eb146e2265988c1ba951ffbc3d3cdb3815b
Abdoulaye Barry <abdoulaye.barry@devopsbootcamp.dev>
2026-04-21 13:15:53 -0400

    Remove app delegate hook and pin iOS target SDK

--- App/ZeroDevOpsApp.swift
+++ App/ZeroDevOpsApp.swift
@@ -8,7 +8,6 @@ import UIKit
 struct ZeroDevOpsApp: App {
 
     @StateObject private var container = AppContainer.shared
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
@@ -19,16 +18,5 @@ struct ZeroDevOpsApp: App {
                     _ = OidcAuthManager.shared.resumeExternalUserAgentFlow(with: url)
                 }
         }
-    }
-}
-
-// MARK: - AppDelegate
-
-final class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        return true
     }
 }
